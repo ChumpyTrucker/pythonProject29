82e3cfadbb9fbb6dbfc3692f4d1419e2b3352bcb
Lilian Istrati <87389714+chumpytrucker@users.noreply.github.com>
2023-07-04 22:25:08 +0100

    Create README.md Add html to the email.

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
 
+html = Template(Path('index.html').read_text())
 email = EmailMessage()
 email['from'] = 'Lilian Istrati'
 email['to'] = 'istratililian1@gmail.com'
 email['subject'] = 'You won 1,000,000 dollars!'
 
-email.set_content('I am a Python Master!')
+email.set_content(html.substitute({'name': 'TinTin'}), 'html')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
 	smtp.ehlo()
 	smtp.starttls()
-	smtp.login('istratililia@gmail.com', '*********')
+	smtp.login('istratililia@gmail.com', '068002425li')
 	smtp.send_message(email)
 	print('all good boss!')
